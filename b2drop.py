5b3997fffc31e9f2a96beaacff65e8dad023888f
Themis Zamani <themiszamani@gmail.com>
2025-02-12 15:48:26 +0200

    Update b2drop.py

--- b2drop.py
+++ b2drop.py
@@ -11,7 +11,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
     handlers=[
-        logging.FileHandler("debug.log"),
+        logging.FileHandler("/var/spool/argo/probes/eudat-b2drop/debug.log"),
         logging.StreamHandler()
     ]
 )
